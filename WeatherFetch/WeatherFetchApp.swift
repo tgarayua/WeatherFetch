61cfa5b9bed5ab842877df5dcabac354cf60bc35
Thomas Garayua <tgarayua@gmail.com>
2024-10-30 01:12:44 -0400

    first commit

---
+++ WeatherFetch/WeatherFetchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherFetchApp.swift
+//  WeatherFetch
+//
+//  Created by Thomas Garayua on 10/30/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherFetchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
